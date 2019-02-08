aba1d0544db493e26227cd8f39018c49b6ff6c27
Pardal <guer0161@algonquinlive.com>
2019-02-08 13:20:12 -0500

    Lab2 commit

--- app/src/main/java/com/example/cst2335/MainActivity.kt
+++ app/src/main/java/com/example/cst2335/MainActivity.kt
@@ -7,6 +7,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main_linear)
+//        setContentView(R.layout.activity_main_grid)
+        setContentView(R.layout.activity_main_relative)
     }
 }
