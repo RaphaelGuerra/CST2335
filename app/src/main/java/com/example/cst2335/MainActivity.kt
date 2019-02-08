b1db4d960819dffd98ea55ed6ea72ed83808dc95
Pardal <guer0161@algonquinlive.com>
2019-02-08 17:48:30 -0500

    Lab3 commit

--- app/src/main/java/com/example/cst2335/MainActivity.kt
+++ app/src/main/java/com/example/cst2335/MainActivity.kt
@@ -1,15 +1,46 @@
 package com.example.cst2335
 
-import android.support.v7.app.AppCompatActivity
+
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sp: SharedPreferences
+    private lateinit var email: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        setContentView(R.layout.activity_main_linear)
-//        setContentView(R.layout.activity_main_grid)
-        setContentView(R.layout.activity_main_relative)
+        setContentView(R.layout.main_lab3)
+        sp = getSharedPreferences("sharedPrefs", 0)
+        email = findViewById(R.id.email)
+        val login: Button = findViewById(R.id.login)
+        login.setOnClickListener{ v -> kotlin.run{openProfileActivity()}}
+        loadData()
     }
+    private fun openProfileActivity(){
+        var nextPage = Intent(this, ProfileActivity::class.java)
+        nextPage.putExtra("sharedPrefs",email.text.toString())
+        startActivity(nextPage)
+    }
+    private fun saveData(){
+        var whatWasTyped = email.text.toString()
+        var editor = sp.edit()
+        editor.putString("text", whatWasTyped)
+        editor.apply()
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
+    }
+    private fun loadData(){
+        var savedString = sp.getString("text", "")
+        email.setText(savedString)
+    }
+    override fun onPause(){
+        super.onPause()
+        saveData()
+    }
+
 }
