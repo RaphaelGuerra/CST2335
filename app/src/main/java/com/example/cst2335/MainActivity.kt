6a4cc32d857c6720cf75ab8048981d2b1b9c1522
Pardal <guer0161@algonquinlive.com>
2019-02-09 09:39:10 -0500

    Lab3 commit

--- app/src/main/java/com/example/cst2335/MainActivity.kt
+++ app/src/main/java/com/example/cst2335/MainActivity.kt
@@ -1,46 +1,46 @@
 package com.example.cst2335
 
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.main.main_lab3.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val SHARED_PREFS = "sharedPrefs"
+        const val TEXT = "text"
+    }
+
     private lateinit var sp: SharedPreferences
-    private lateinit var email: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_lab3)
-        sp = getSharedPreferences("sharedPrefs", 0)
-        email = findViewById(R.id.email)
-        val login: Button = findViewById(R.id.login)
-        login.setOnClickListener{ v -> kotlin.run{openProfileActivity()}}
+        sp = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        login.setOnClickListener{ openProfileActivity()}
         loadData()
     }
     private fun openProfileActivity(){
-        var nextPage = Intent(this, ProfileActivity::class.java)
-        nextPage.putExtra("sharedPrefs",email.text.toString())
+        val nextPage = Intent(this, ProfileActivity::class.java)
+        nextPage.putExtra(SHARED_PREFS , email.text.toString())
         startActivity(nextPage)
     }
     private fun saveData(){
-        var whatWasTyped = email.text.toString()
-        var editor = sp.edit()
-        editor.putString("text", whatWasTyped)
+        val editor = sp.edit()
+        editor.putString(TEXT, email.text.toString())
         editor.apply()
         Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
     }
     private fun loadData(){
-        var savedString = sp.getString("text", "")
-        email.setText(savedString)
+        email.setText(sp.getString(TEXT, ""))
     }
     override fun onPause(){
         super.onPause()
         saveData()
     }
-
 }
